b2037764047ad9a9569516237567e506d05d648c
Andrew Lai <andrewlai@Andrews-MacBook-Air.local>
2024-10-09 16:49:13 +0800

    Update transactions backend to call alchemy api for transactions for the wallet

--- src/pages/api/transactions.ts
+++ src/pages/api/transactions.ts
@@ -1,25 +1,63 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Alchemy, AssetTransfersCategory, AssetTransfersWithMetadataResult, Network } from 'alchemy-sdk';
 
-const transactions = [
-  { id: 1, from: '0x1c802a166f23d37f4c321f1c08c650cb6a898dcf', to: '0x7c116883478cfa36f91b4c314dabe90f8405282a', amount: '0.1', timestamp: Date.now() },
-  { id: 2, from: '0x1c802a166f23d37f4c321f1c08c650cb6a898dcf', to: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045', amount: '0.12', timestamp: Date.now() },
-  { id: 3, from: '0x1c802a166f23d37f4c321f1c08c650cb6a898dcf', to: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045', amount: '0.11', timestamp: Date.now() },
-];
+const settings = {
+  apiKey: process.env.ALCHEMY_API_KEY,
+  network: Network.ETH_SEPOLIA,
+};
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const alchemy = new Alchemy(settings);
+
+async function fetchTransactions(address: string) {
+  try {
+    const sentTransfers = await alchemy.core.getAssetTransfers({
+      fromAddress: address,
+      fromBlock: '0x0',
+      category: [AssetTransfersCategory.EXTERNAL],
+      withMetadata: true,
+    });
+    const receivedTransfers = await alchemy.core.getAssetTransfers({
+      toAddress: address,
+      fromBlock: '0x0',
+      category: [AssetTransfersCategory.EXTERNAL],
+      withMetadata: true,
+    });
+    const allTransfers = [...sentTransfers.transfers, ...receivedTransfers.transfers];
+    return allTransfers.map(formatTransaction).sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+
+  } catch (error) {
+    console.error('Error fetching transactions:', error);
+    return [];
+  }
+}
+
+const formatTransaction = (transaction: AssetTransfersWithMetadataResult) => {
+  return {
+    txHash: transaction.hash,
+    from: transaction.from,
+    to: transaction.to,
+    amount: transaction.value,
+    timestamp: new Date(transaction.metadata.blockTimestamp).toLocaleString(),
+  };
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    res.status(200).json(transactions);
+    const allTransactions = await fetchTransactions(req.query.address as string);
+
+    res.status(200).json(allTransactions);
   } else if (req.method === 'POST') {
-    const { from, to, amount } = req.body;
-    const newTransaction = {
-      id: transactions.length + 1,
-      from,
-      to,
-      amount,
-      timestamp: Date.now(),
-    };
-    transactions.push(newTransaction);
-    res.status(201).json(newTransaction);
+    // not use
+    // const { from, to, amount } = req.body;
+    // const newTransaction = {
+    //   id: transactions.length + 1,
+    //   from,
+    //   to,
+    //   amount,
+    //   timestamp: Date.now(),
+    // };
+    // transactions.push(newTransaction);
+    res.status(201).json([]);
   } else {
     res.setHeader('Allow', ['GET', 'POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
